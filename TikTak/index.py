1f4eb42888c6c0505565384741b3f1c9cc8f16cb
Oana Fatu <oana@alvalabs.io>
2020-01-10 10:45:25 +0100

    improve tiktak code

--- TikTak/index.py
+++ TikTak/index.py
@@ -1,5 +1,6 @@
 import random
 
+
 def clear_screen():
     print('\n' * 100)
 
@@ -14,6 +15,7 @@ def display_board(board):
 
 def player_assignment():
     user_input = input("Do you want to play with 'X' or with '0'? \n")
+
     while user_input not in ['X', '0', 'x']:
         print("Input is not correct, please enter 'X' or '0' \n")
         user_input = input("Do you want to play with 'X' or with '0'? \n")
@@ -42,43 +44,58 @@ def update_board(marker, input, board):
 def what_player_starts():
     return random.randint(1, 2)
 
+
 def win_check(board, marker):
+
     if (board[1] == board[2] == board[3] == marker) or (board[4] == board[5] == board[6] == marker) or \
             (board[7] == board[8] == board[9] == marker) or (board[1] == board[5] == board[9] == marker) or \
             (board[3] == board[5] == board[7] == marker):
         print(f"Player '{marker}' won!")
+        ask_rematch()
         return True
     else:
         return False
 
 
-players = player_assignment()
-player_1_marker = players[1]
-player_2_marker = players[2]
-print(players)
-empty_board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-board = empty_board
+def ask_rematch():
+    play_again = input('Would you like to play again? (yes/no)\n')
 
-player_to_start = what_player_starts()
-print(f"The player to start was randomly picked to be {player_to_start}, playing with '{players[player_to_start]}'")
-display_board(board)
+    while play_again not in ['yes', 'no']:
+        play_again = input('Would you like to play again? (yes/no)\n')
+    if play_again == 'yes':
+        game_on()
+    else:
+        print('Thanks for playing!')
 
-game_over = False
-current_player_marker = players[player_to_start]
-moves = 1
 
-while game_over is False and moves < 10:
+def game_on():
+    clear_screen()
+    players = player_assignment()
+    print(players)
+    empty_board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    board = empty_board
 
-    move_to_play = player_input(current_player_marker, board)
-    board = update_board(current_player_marker, move_to_play, board)
+    player_to_start = what_player_starts()
+    print(f"The player to start was randomly picked to be {player_to_start}, playing with '{players[player_to_start]}'")
     display_board(board)
-    moves += 1
-    game_over = win_check(board, current_player_marker)
-    current_player_marker = 'X' if current_player_marker == '0' else '0'
 
-print(moves)
-if moves == 10:
-    print('The game is over, no-one won, no-one lost! It was a tie!')
+    game_over = False
+    current_player_marker = players[player_to_start]
+    moves = 1
+
+    while game_over is False and moves < 10:
+
+        move_to_play = player_input(current_player_marker, board)
+        board = update_board(current_player_marker, move_to_play, board)
+        display_board(board)
+        moves += 1
+        game_over = win_check(board, current_player_marker)
+        current_player_marker = 'X' if current_player_marker == '0' else '0'
+
+    if moves == 10:
+        print('The game is over, no-one won, no-one lost! It was a tie!')
+        ask_rematch()
 
 
+game_on()
 
